6fe808d00c7435f494869bcc225dd78d540ae1b5
Lucas Nunes <lucasnunes000@gmail.com>
2015-09-10 17:18:41 -0300

    Atualizacoes

--- js/masonryApp.js
+++ js/masonryApp.js
@@ -1,29 +1,74 @@
 angular.module('masonryApp', ['wu.masonry']).
-                controller('DemoCtrl', function ($scope) {
-                    function genBrick() {
-                        var height = ~~(Math.random() * 500) + 100;
-                        var id = ~~(Math.random() * 10000);
-                        return {
-                            src: 'http://lorempixel.com/g/280/' + height + '/?' + id
-                        };
-                    };
+    controller('DemoCtrl', function ($scope) {
 
-                    $scope.bricks = [
-                        genBrick(),
-                        genBrick(),
-                        genBrick(),
-                        genBrick(),
-                        genBrick()
-                    ];
+        var leitorDeCSV = new FileReader()
 
-                    $scope.add = function add() {
-                        $scope.bricks.push(genBrick());
-                    };
+        window.onload = function init() {
+            leitorDeCSV.onload = leCSV;
+        }
 
-                    $scope.remove = function remove() {
-                        $scope.bricks.splice(
-                            ~~(Math.random() * $scope.bricks.length),
-                            1
-                        )
-                    };
-                });
+        function genBrick() {
+            var height = ~~(Math.random() * 500) + 100;
+            var id = ~~(Math.random() * 10000);
+            return {
+                src: 'img/ic-ok.png'
+            };
+        };
+
+        $scope.bricks = [
+            genBrick(),
+            genBrick(),
+            genBrick(),
+            genBrick(),
+            genBrick()
+        ];
+
+
+        $scope.pegarArquivo = function pegarArquivo(arquivoSelecionado) {
+            var imgLoca = document.getElementById('imgLocal');
+            var file = files[0];
+            var img = document.createElement("img");
+                img.file = file;
+                imgLocal.appendChild(img)
+         
+            var reader = new FileReader();
+                reader.onload = (function(aImg) {return function(e) {aImg.src = e.target.result;};})(img);
+                reader.readAsDataURL(file);
+        };
+
+        $scope.pegaCSV = function pegaCSV(inputFile) {
+            var file = inputFile.files[0];
+            leitorDeCSV.readAsText(file);
+        };
+
+
+        $scope.leCSV = function leCSV() {
+            var fileArr = evt.target.result.split('\n');
+            var strDiv = '<table>';
+
+            for (var i=0; i<fileArr.length; i++) {
+                strDiv += '<tr>';
+                var fileLine = fileArr[i].split(',');
+                    for (var j=0; j<fileLine.length; j++) {
+                        strDiv += '<td>'+fileLine[j].trim()+'</td>';
+                    }
+                strDiv += '</tr>';
+            }
+             
+            strDiv += '</table>';
+            var CSVsaida = document.getElementById('CSVsaida');
+            CSVsaida.innerHTML = strDiv;
+        };
+
+        $scope.add = function add() {
+            $scope.bricks.push(genBrick());
+        };
+
+        $scope.remove = function remove() {
+            $scope.bricks.splice(
+                ~~(Math.random() * $scope.bricks.length),
+                1
+            )
+        };
+    });
+
