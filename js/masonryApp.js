0e10af1e76110d256774569e531bcdcd21055219
Lucas Nunes <lucasnunes@MacBook-Air-de-Lucas.local>
2015-09-11 14:28:20 -0300

    Removendo codigo desnecessario

--- js/masonryApp.js
+++ js/masonryApp.js
@@ -1,13 +1,7 @@
 angular.module('masonryApp', ['wu.masonry']).
     controller('DemoCtrl', function ($scope) {
 
-        var leitorDeCSV = new FileReader()
-
-        window.onload = function init() {
-            leitorDeCSV.onload = leCSV;
-        }
-
-        function genBrick() {
+       function genBrick() {
             var height = ~~(Math.random() * 500) + 100;
             var id = ~~(Math.random() * 10000);
             return {
@@ -21,44 +15,7 @@ angular.module('masonryApp', ['wu.masonry']).
             genBrick(),
             genBrick(),
             genBrick()
-        ];
-
-
-        $scope.pegarArquivo = function pegarArquivo(arquivoSelecionado) {
-            var imgLoca = document.getElementById('imgLocal');
-            var file = files[0];
-            var img = document.createElement("img");
-                img.file = file;
-                imgLocal.appendChild(img)
-         
-            var reader = new FileReader();
-                reader.onload = (function(aImg) {return function(e) {aImg.src = e.target.result;};})(img);
-                reader.readAsDataURL(file);
-        };
-
-        $scope.pegaCSV = function pegaCSV(inputFile) {
-            var file = inputFile.files[0];
-            leitorDeCSV.readAsText(file);
-        };
-
-
-        $scope.leCSV = function leCSV() {
-            var fileArr = evt.target.result.split('\n');
-            var strDiv = '<table>';
-
-            for (var i=0; i<fileArr.length; i++) {
-                strDiv += '<tr>';
-                var fileLine = fileArr[i].split(',');
-                    for (var j=0; j<fileLine.length; j++) {
-                        strDiv += '<td>'+fileLine[j].trim()+'</td>';
-                    }
-                strDiv += '</tr>';
-            }
-             
-            strDiv += '</table>';
-            var CSVsaida = document.getElementById('CSVsaida');
-            CSVsaida.innerHTML = strDiv;
-        };
+        ];      
 
         $scope.add = function add() {
             $scope.bricks.push(genBrick());
